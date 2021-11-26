a813192ee26592869f2803b424048c345bb9403c
Mark <61845375+MrMarL@users.noreply.github.com>
2021-11-26 20:15:17 +0300

    2-work

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program:A
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Vvedi ");
+            Console.WriteLine(GetUr(1,1));
+            Console.ReadKey();*/
+        }
+    }
+}
